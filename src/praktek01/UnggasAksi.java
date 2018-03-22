1013db2eb59db220d3a40196c4267b1df5277e5c
ahmadridhosetiawan <32403430+ahmadridhosetiawan@users.noreply.github.com>
2018-03-22 17:56:10 +0800

    Mengisi Nilai Attribut

--- src/praktek01/UnggasAksi.java
+++ src/praktek01/UnggasAksi.java
@@ -12,9 +12,43 @@ package praktek01;
 public class UnggasAksi {
     public static void main(String[] args) {
         Unggas Bebek= new Unggas();
+        
+        Bebek.nama="Bebek Gendut";
+        Bebek.bentukkaki="Tumpul Lebar";
+        Bebek.makanan="Dadak";
+        Bebek.warnabulu="Coklat";
+        Bebek.warnaparuh="Kuning";
+        
         Unggas Ayam= new Unggas();
+        
+        Ayam.nama="Ayam Kurus";
+        Ayam.bentukkaki="Tajam";
+        Ayam.makanan="Beras";
+        Ayam.warnabulu="PutihKuning";
+        Ayam.warnaparuh="Kuning";
+        
         Unggas Angsa= new Unggas();
+        
+        Angsa.nama="Angsa Cantik";
+        Angsa.bentukkaki="Tumpul Lebar";
+        Angsa.makanan="Nasi";
+        Angsa.warnabulu="Putih";
+        Angsa.warnaparuh="Kuning";
+        
         Unggas Elang= new Unggas();
+        
+        Elang.nama="Elang Ganteng ";
+        Elang.bentukkaki="Tajam";
+        Elang.makanan="Tikus";
+        Elang.warnabulu="Coklat";
+        Elang.warnaparuh="Kuning";
+        
         Unggas Gagak= new Unggas();
+        
+        Gagak.nama="Gagak Jahat";
+        Gagak.bentukkaki="Tajam";
+        Gagak.makanan="Bangkai";
+        Gagak.warnabulu="Hitam";
+        Gagak.warnaparuh="Hitam";
     }
 }
