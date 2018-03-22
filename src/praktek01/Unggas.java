b9c3a3fd023f435cb3f36693b5c7401a15ce7e00
ahmadridhosetiawan <32403430+ahmadridhosetiawan@users.noreply.github.com>
2018-03-22 17:35:00 +0800

    Membuat Attribut dan Behavior

--- src/praktek01/Unggas.java
+++ src/praktek01/Unggas.java
@@ -10,5 +10,19 @@ package praktek01;
  * @author edong
  */
 public class Unggas {
+    String nama;
+    String warnaparuh;
+    String makanan;
+    String bentukkaki;
+    String warnabulu;
+    
+    void cetakInfo(){
+        System.out.println("Nama:"+nama);
+        System.out.println("Warna Paruh:"+warnaparuh);
+        System.out.println("Makanan:"+makanan);
+        System.out.println("Bentuk Kaki:"+bentukkaki);
+        System.out.println("Warna Bulu:"+warnabulu); 
+    }
+    
     
 }
